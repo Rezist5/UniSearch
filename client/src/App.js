ab8b433fa2e5730850c73dd964c9cc45245f0fa4
Rezist5 <dudikov.ivanchik@gmail.com>
2024-04-29 22:58:26 +0500

    Third

--- client/src/App.js
+++ client/src/App.js
@@ -2,24 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const {user} = useContext(Context)
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        check().then(data => {
+            user.setUser(true)
+            user.setIsAuth(true)
+        }).finally(() => setLoading(false))
+    }, [])
+
+    if (loading) {
+        return <Spinner animation={"grow"}/>
+    }
+
+    return (
+        <BrowserRouter>
+            <NavBar />
+            <AppRouter />
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ client/src/routes.js
@@ -0,0 +1,40 @@
+import Admin from "./pages/Admin";
+import {ADMIN_ROUTE, PORTFOLIO_ROUTE, UNIVERSITY_ROUTE, REPRESENTATIVE_ROUTE, LOGIN_ROUTE, REGISTRATION_ROUTE, MAIN_ROUTE} from "./utils/consts";
+import Portfolio from "./pages/Basket";
+import Main from "./pages/Shop";
+import Auth from "./pages/Auth";
+import UniversityPage from "./pages/DevicePage";
+
+export const authRoutes = [
+    {
+        path: ADMIN_ROUTE,
+        Component: Admin
+    },
+    {
+        path: PORTFOLIO_ROUTE,
+        Component: Portfolio
+    },
+    {
+        path: REPRESENTATIVE_ROUTE,
+        Component: Representative
+    },
+]
+
+export const publicRoutes = [
+    {
+        path: MAIN_ROUTE,
+        Component: Main
+    },
+    {
+        path: LOGIN_ROUTE,
+        Component: Auth
+    },
+    {
+        path: REGISTRATION_ROUTE,
+        Component: Auth
+    },
+    {
+        path: UNIVERSITY_ROUTE + '/:id',
+        Component: UniversityPage
+    },
+]
